343d550dcb5e661d2ec6f7b95545f1657ce3b3f0
Nas3ts <nkisowile@gmail.com>
2025-05-21 03:09:22 +0300

    fix: repair signin page layout

--- src/components/auth/SignInForm.jsx
+++ src/components/auth/SignInForm.jsx
@@ -6,8 +6,8 @@ export default function SignInForm() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
-      <div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
         <div className="mb-5 sm:mb-8">
           <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
             Welcome Back!
