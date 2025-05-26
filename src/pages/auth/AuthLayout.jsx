fa9a4f0919d6d9ab43db7eb532047376007d13aa
Nas3ts <nkisowile@gmail.com>
2025-05-26 13:38:36 +0300

    feat: add auth layout

---
+++ src/pages/auth/AuthLayout.jsx
@@ -0,0 +1,9 @@
+export default function AuthLayout({ children }) {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
+                {children}
+            </div>
+        </div>
+    );
+}
